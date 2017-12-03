66cc3433c9abcdb47b43bfeb6ca3ca03e984dbc2
churkinv <churkinv@gmail.com>
2017-12-03 22:21:16 +0200

    Percolation Problem initialized

---
+++ src/algorithms/Algorithms.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithms;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+/**
+ *
+ * @author Raccoon
+ */
+public class Algorithms {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
